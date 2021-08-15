89f59782e1f58aeb497520960b94bced2f8c8810
Siam-al-islam <siamalislam692@gmail.com>
2021-08-15 21:08:16 +0600

    sugv

---
+++ js/banking.js
@@ -0,0 +1,11 @@
+//handle deposit button
+document.getElementById('deposit-button').addEventListener('click', function () {
+    // get the amount deposited 
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
+
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = depositAmount;
+    depositInput.value = '';
+})
--- js/login.js
+++ js/login.js
@@ -11,14 +11,4 @@ document.getElementById('login-submit').addEventListener('click', function () {
     }
 })
 
-//handle deposit button
-document.getElementById('deposit-button').addEventListener('click', function () {
-    // get the amount deposited 
-    const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
 
-    const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
-    depositInput.value = '';
-})
