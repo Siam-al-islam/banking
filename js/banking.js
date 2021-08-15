30f09a0d04d2539fa1220099386b61795d393a21
Siam-al-islam <siamalislam692@gmail.com>
2021-08-15 22:59:16 +0600

    finished

--- js/banking.js
+++ js/banking.js
@@ -22,4 +22,30 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
     //clear the deposit input field
     depositInput.value = '';
+});
+
+// handle withdraw event handler    
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+    // set withdraw total 
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWwithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWwithdrawTotal;
+
+    //update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    //clear the withdraw input field
+    withdrawInput.value = '';
 })
